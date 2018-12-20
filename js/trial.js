5ea9dcd715b4ddbbb919b0d3dd7b01ef6a08424a
jing.dong <dj3386083@gmail.com>
2018-12-20 12:32:10 +1100

    hahaha

--- js/trial.js
+++ js/trial.js
@@ -14,14 +14,14 @@ $(document).ready(function (){
 
 
 let turnsPlayed = 0;
-let playOneIsNext = true;
-// let boxId = 0;
-// let xWin = 0;
-// let oWin = 0;
+let playerOne = true;
 
 let board =["-", "-", "-",
           "-", "-", "-",
           "-", "-", "-" ];
+
+let player1 = "X";
+let player2 = "O";
 
 const renderToScreen = function ( boxId ){   //put contecnt into board
   for (var i = 0; i < board.length; i++) {
@@ -39,60 +39,74 @@ const reset = function (){ // reset game
             "-", "-", "-" ];
   turnsPlayed = 0; // clear origin playturn
   renderToScreen();
-  //location.reload(); //
+  //location.reload();
 };
 
 
 
 const playTurn = function(num) {
-  if (board [num] === "X" || board [num] === "O") { //check is there x or o in cureent board, if there is x or o in board,
+  if (board [num] === player1 || board [num] === player2) { //check is there x or o in cureent board, if there is x or o in board,
     return ;        // you cannot change to other value.
   }
 
-  if (playOneIsNext) { //player x turn, when value is true will go to next line.
-    board [num] = "X"
+  if (playerOne) { //player x turn, when value is true will go to next line.
+    //board [num] = "X"
+    board [num] = player1;
     renderToScreen ();
     turnsPlayed = turnsPlayed + 1;
-    playOneIsNext = false
-    checkForWin ("X")
+    playerOne = false
+    //checkForWin ("X")
+    checkForWin (player1)
 
     if ( turnsPlayed  === 9 && (checkForWin("X") !== true) ) {
       //console.log("draw");
-      alert("You should practice more ~~")
+      alert("Oh no, it's a draw")
     }
 
   } else {
-
-    board [num ] = "O"
+    //board [num ] = "O"
+    board [num] = player2;
     renderToScreen ();
     turnsPlayed = turnsPlayed + 1;
     //playTwoIsNext = true
-    playOneIsNext = true
-    checkForWin ("O")
+    playerOne = true
+    //checkForWin ("O")
+    checkForWin (player2)
   }
-  //checkForWin ()
 }
 
 
 
+const checkForWin = function (player1, player2) {
 
-const checkForWin = function (player) {
   if (
-    (board[0] === player && board[1] === player && board[2] === player) ||
-    (board[3] === player && board[4] === player && board[5] === player) ||
-    (board[6] === player && board[7] === player && board[8] === player) ||
-    (board[0] === player && board[3] === player && board[6] === player) ||
-    (board[1] === player && board[4] === player && board[7] === player) ||
-    (board[2] === player && board[5] === player && board[8] === player) ||
-    (board[0] === player && board[4] === player && board[8] === player) ||
-    (board[2] === player && board[4] === player && board[6] === player)
+    (board[0] === player1 && board[1] === player1 && board[2] === player1) ||
+    (board[3] === player1 && board[4] === player1 && board[5] === player1) ||
+    (board[6] === player1 && board[7] === player1 && board[8] === player1) ||
+    (board[0] === player1 && board[3] === player1 && board[6] === player1) ||
+    (board[1] === player1 && board[4] === player1 && board[7] === player1) ||
+    (board[2] === player1 && board[5] === player1 && board[8] === player1) ||
+    (board[0] === player1 && board[4] === player1 && board[8] === player1) ||
+    (board[2] === player1 && board[4] === player1 && board[6] === player1)
 
   ) {
     //console.log("winner");
-    alert ("winer winer chicken dinner")
-    return true;
+    alert (`winner is ${player1}`)
+    //return true;
+  } else if (
+    (board[0] === player2 && board[1] === player2 && board[2] === player2) ||
+    (board[3] === player2 && board[4] === player2 && board[5] === player2) ||
+    (board[6] === player2 && board[7] === player2 && board[8] === player2) ||
+    (board[0] === player2 && board[3] === player2 && board[6] === player2) ||
+    (board[1] === player2 && board[4] === player2 && board[7] === player2) ||
+    (board[2] === player2 && board[5] === player2 && board[8] === player2) ||
+    (board[0] === player2 && board[4] === player2 && board[8] === player2) ||
+    (board[2] === player2 && board[4] === player2 && board[6] === player2)
+  ){
+    alert (`winner is ${player2}`)
   }
-}
+
+};
 
 
 $(document).ready( function() { //主程序，当玩家点击任意board的时候，运行playturn的function，直到该function结束。
