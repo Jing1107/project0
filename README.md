9ad6c9be43094907feeca850603a5aff81c682f1
jing.dong <dj3386083@gmail.com>
2018-12-21 00:06:38 +1100

    update readme, and try to add two buttons

--- README.md
+++ README.md
@@ -1,87 +1,33 @@
-# Project #0: Tic tac toe
+# Project #0
+## Tic tac toe
 
-About the Project
-Here is the live demo
-[Click here] (https://jing1107.github.io/project0/)
+### About the Project
+Here is the live demo []
+https://jing1107.github.io/project0/ []
 for the live site.
 
-This is the first game I've ever programmed. The below skills were tested.
 
-Command line
-Source control
-Deployment
-It's a simple two player game of TicTacToe built with HTML, CSS, Javascript and the jQuery library.
+
+#### This is the first game I've ever programmed. The below skills were tested.
+
+- Command line
+- Source control
+- Deployment
+- It's a simple two player game of TicTacToe built with HTML, CSS, Javascript and the jQuery library.
 
 The basic requirement is to have the game switch turns between X and 0 and visually display which side won if a player gets three in a row or a draw is found.
 
-Bonus functionality:
 
-Animations including an looping gradient background
-Fade in and fade out of player icons
-Modal displaying which player has won and a restart button
-Future improvements:
+### Bonus functionality:
+- [ ] Animations including an looping gradient background
 
+- [ ] Fade in and fade out of player icons
+
+- [x]Modal displaying which player has won and a restart     button
+
+### Future improvements:
 Review code and make it DRYer
-LocalStorage
+
 Ability for players to customise their tokens
 
-
-The Game
-=============
-
-Overview
----------
-
-- AAAA
-- BBBB
-
-
-## paragraphs
-
-No markup
-
-Just new lines
-
-Isn't that nice?
-
-> I never said that  ---- Richard
-
-Within a paragraph we can *emphasise* certain words or
-we can **bold** them. Alternatively: _this_ or
-_this_ and even ***this***
-
-## lists
-
-- Group
-- Hero
-- Chico
-
-### Also
-
-* a
-* b
-* c
-
-
-### and
-
-1. a
-2. b
-3. c
-
-
-## Links
-We can easily create [links] {http://github.com}
-
-## Live Demo
-[Click here] (http://wofockham.github.io/furry-system)
-for the live site.
-
-## Image
-![A picture of Bill Murray]
-(http://fullmurray.com/50/50)
-
-## Code
-
-You can markdown a specific like so: please do not use <marquee>
-github flavour markdown
+Adding AI version for only one player
--- js/trial.js
+++ js/trial.js
@@ -12,45 +12,29 @@ $(document).ready(function (){
   });
 }); // make h1 colorful
 
-
-let turnsPlayed = 0;
-let playerOne = true;
-
-let board =["-", "-", "-",
+const board =["-", "-", "-",
           "-", "-", "-",
           "-", "-", "-" ];
 
+let turnsPlayed = 0;
+let playerOne = true;
 let player1 = "X";
 let player2 = "O";
 
+const xPlayer = function (num) {
+   board[num] = player1;
+}
+
+const oPlayer = function (num) {
+  board[num] = player2;
+};
 
 const renderToScreen = function ( boxId ){   //put contecnt into board
   for (var i = 0; i < board.length; i++) {
     $('#' + i).text(board[i])
     //console.log (board[i])
-  }//liesten click
+  } //liesten click
 }
-
-const playerX = function (num) {
-   //console.log(player1);
-   board[num] = player1;
-}
-
-const player0 = function (num) {
-  //console.log(player2);
-  board[num] = player2;
-}
-
-const reset = function (){ // reset game
-  board =["-", "-", "-",
-            "-", "-", "-",
-            "-", "-", "-" ];
-  turnsPlayed = 0; // clear origin playturn
-  renderToScreen();
-  //location.reload();
-};
-
-
 
 const playTurn = function(num) {
   if (board [num] === player1 || board [num] === player2) { //check is there x or o in cureent board, if there is x or o in board,
@@ -59,26 +43,33 @@ const playTurn = function(num) {
 
   if (playerOne) { //player x turn, when value is true will go to next line.
     board [num] = player1;
-    //board [num] = playerX;
     renderToScreen ();
     turnsPlayed = turnsPlayed + 1;
     playerOne = false
     checkForWin (player1)
-    //checkForWin (playerX)
+
+  // if (board[num] === " ") {
+  // if (playerOne === true ) {
+  // xPlayer(num);
+  // turnsPlayed += 1;
+  // playerOne = false;
 
 
     if ( turnsPlayed  === 9 && (checkForWin(player1) !== true) ){
       alert("Oh no, it's a draw")
+      //$('.winner').text(`${ player } wins!`)
     }
 
   } else {
     board [num] = player2;
-    //board [num] = playerO;
     renderToScreen ();
     turnsPlayed = turnsPlayed + 1;
     playerOne = true
     checkForWin (player2)
-    //checkForWin (playerO)
+
+    // oPlayer(num);
+    // turnsPlayed += 1;
+    // playerOne = true;
   }
 }
 
@@ -97,9 +88,9 @@ const checkForWin = function (player1, player2) {
     (board[2] === player1 && board[4] === player1 && board[6] === player1)
 
   ) {
-    //console.log("winner");
-    alert (`winner is ${player1}`)
+    $('.winner').text(`${player1} WINS !`)
     return true;
+
   } else if (
     (board[0] === player2 && board[1] === player2 && board[2] === player2) ||
     (board[3] === player2 && board[4] === player2 && board[5] === player2) ||
@@ -110,14 +101,23 @@ const checkForWin = function (player1, player2) {
     (board[0] === player2 && board[4] === player2 && board[8] === player2) ||
     (board[2] === player2 && board[4] === player2 && board[6] === player2)
   ){
-    alert (`winner is ${player2}`)
+    $('.winner').text(`${player2} WINS !`)
     return true;
+
   } else {
     return false;
   }
 
 };
 
+const reset = function (){ // reset game
+  // board =["-", "-", "-",
+  //           "-", "-", "-",
+  //           "-", "-", "-" ];
+  // turnsPlayed = 0; // clear origin playturn
+  // renderToScreen();
+  location.reload();
+};
 
 $(document).ready( function() { //主程序，当玩家点击任意board的时候，运行playturn的function，直到该function结束。
   //console.log("ready");
@@ -152,10 +152,10 @@ $(document).ready( function() { //主程序，当玩家点击任意board的时�
   $('.reset').click( function(){
     reset()
   })
-  $('#playerX').click( function (){
-    playerX(1)
-  })
-  $('#playerO').click( function (){
-    playerO(1)
-  })
+  // $('#X').click( function (){
+  //   xPlayer()
+  // })
+  // $('#O').click( function (){
+  //   oPlayer()
+  // })
 })
